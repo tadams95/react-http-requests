a1f5099a51e8490926b9cc0b41d66be0113a11cf
tadams95 <tyrelleadams28@gmail.com>
2022-11-17 20:58:17 -0800

    Using async / await, title change

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
+  const [movies, setMovies] = useState([]);
 
-const [movies, setMovies] = useState([]);
+  //using async and await functions
+  async function fetchMovieHandler() {
+    const response = await fetch("https://swapi.dev/api/films");
+    const data = await response.json();
 
-  function fetchMovieHandler() {
-    fetch("https://swapi.dev/api/films").then(response => {
-      return response.json();
-    }).then(data => {
-      const transformedMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        };
-      })
-     setMovies(transformedMovies);
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
     });
+    setMovies(transformedMovies);
   }
 
   return (
